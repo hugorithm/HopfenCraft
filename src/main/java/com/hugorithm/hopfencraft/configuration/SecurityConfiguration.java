b243a6df3d5db1ee0c5e6ae6ed6d6216f3f38612
hugorithm <hugofsilv@gmail.com>
2023-08-31 21:31:27 +0100

    Add user model, controller and service

--- src/main/java/com/hugorithm/hopfencraft/configuration/SecurityConfiguration.java
+++ src/main/java/com/hugorithm/hopfencraft/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.hugorithm.hopfencraft.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,7 +11,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return  http.
-                csrf(csrf -> csrf.disable())
+                csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .build();
     }
