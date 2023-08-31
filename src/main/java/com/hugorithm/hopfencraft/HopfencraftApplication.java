8bf6f4cab2c5e8fd8a5f2887a3784a128ad917e4
hugorithm <hugofsilv@gmail.com>
2023-08-31 20:43:58 +0100

    1st commit

---
+++ src/main/java/com/hugorithm/hopfencraft/HopfencraftApplication.java
@@ -0,0 +1,13 @@
+package com.hugorithm.hopfencraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopfencraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopfencraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugorithm/hopfencraft/HopfencraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugorithm.hopfencraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopfencraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
