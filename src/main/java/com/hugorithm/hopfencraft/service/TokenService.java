63b8751ee4f97eb5f75b9c1823800bd5beba0acd
hugorithm <hugofsilv@gmail.com>
2023-09-01 19:41:43 +0100

    add token expiration

--- src/main/java/com/hugorithm/hopfencraft/service/TokenService.java
+++ src/main/java/com/hugorithm/hopfencraft/service/TokenService.java
@@ -1,9 +1,8 @@
 package com.hugorithm.hopfencraft.service;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
@@ -24,16 +23,17 @@ public class TokenService {
     }
 
     public String generateJwt(Authentication auth){
-
         Instant now = Instant.now();
-
         String scope = auth.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(" "));
 
+        Instant expires = now.plus(Duration.ofHours(2));
+
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer("self")
                 .issuedAt(now)
+                .expiresAt(expires)
                 .subject(auth.getName())
                 .claim("roles", scope)
                 .build();
