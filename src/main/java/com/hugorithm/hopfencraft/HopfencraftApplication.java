5e495efa74fc0da88fa190aa9fc47aacf32bd2d6
hugorithm <hugofsilv@gmail.com>
2023-09-05 21:43:13 +0100

    Add Async

--- src/main/java/com/hugorithm/hopfencraft/HopfencraftApplication.java
+++ src/main/java/com/hugorithm/hopfencraft/HopfencraftApplication.java
@@ -10,12 +10,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.*;
-
+@EnableAsync
 @SpringBootApplication
 public class HopfencraftApplication {
 
