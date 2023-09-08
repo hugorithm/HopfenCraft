56f5301fa66ffbe0a6a317f031364e3140b6f6d0
hugorithm <hugofsilv@gmail.com>
2023-09-08 19:42:37 +0100

    add RsaKeyGenerationException

---
+++ src/main/java/com/hugorithm/hopfencraft/exception/RsaKeyGenerationException.java
@@ -0,0 +1,7 @@
+package com.hugorithm.hopfencraft.exception;
+
+public class RsaKeyGenerationException extends RuntimeException{
+    public RsaKeyGenerationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/hugorithm/hopfencraft/utils/KeyGeneratorUtility.java
+++ src/main/java/com/hugorithm/hopfencraft/utils/KeyGeneratorUtility.java
@@ -1,9 +1,15 @@
 package com.hugorithm.hopfencraft.utils;
 
+import com.hugorithm.hopfencraft.exception.RsaKeyGenerationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 
 public class KeyGeneratorUtility {
+    private final static Logger LOGGER = LoggerFactory.getLogger(KeyGeneratorUtility.class);
     public static KeyPair generateRsaKey(){
         KeyPair kp;
 
@@ -11,8 +17,10 @@ public class KeyGeneratorUtility {
             KeyPairGenerator kpGen = KeyPairGenerator.getInstance("RSA");
             kpGen.initialize(2048);
             kp = kpGen.generateKeyPair();
-        } catch (Exception e){
-            throw new IllegalStateException();
+
+        } catch (NoSuchAlgorithmException ex){
+            LOGGER.error(ex.getMessage(), ex);
+            throw new RsaKeyGenerationException("Failed to generate RSA key pair due to NoSuchAlgorithmException");
         }
 
         return kp;
