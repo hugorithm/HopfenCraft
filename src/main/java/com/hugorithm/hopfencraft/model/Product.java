a26ea0ad9f0a5f3e25a963868b42b39ee17a9a0f
hugorithm <hugofsilv@gmail.com>
2023-09-09 13:20:37 +0100

    remove field

--- src/main/java/com/hugorithm/hopfencraft/model/Product.java
+++ src/main/java/com/hugorithm/hopfencraft/model/Product.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -27,7 +26,6 @@ public class Product {
     private int quantity;
     private BigDecimal price;
     @CreationTimestamp
-    @UpdateTimestamp
     private LocalDateTime registerDateTime;
 
 
