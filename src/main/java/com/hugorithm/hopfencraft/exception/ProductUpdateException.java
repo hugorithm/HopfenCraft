6958b0ad5e672f9c7b1d5eca30ff98f9d9d6b49b
hugorithm <hugofsilv@gmail.com>
2023-09-09 12:07:28 +0100

    add ProductUpdate exception

---
+++ src/main/java/com/hugorithm/hopfencraft/exception/ProductUpdateException.java
@@ -0,0 +1,7 @@
+package com.hugorithm.hopfencraft.exception;
+
+public class ProductUpdateException extends RuntimeException{
+    public ProductUpdateException(String message) {
+        super(message);
+    }
+}
