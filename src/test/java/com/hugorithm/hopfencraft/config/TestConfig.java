a3e4f9b54268edc5cf7ef420863041861e7f3195
hugorithm <hugofsilv@gmail.com>
2023-09-10 23:42:22 +0100

    add TestConfig

---
+++ src/test/java/com/hugorithm/hopfencraft/config/TestConfig.java
@@ -0,0 +1,41 @@
+package com.hugorithm.hopfencraft.config;
+
+import com.hugorithm.hopfencraft.repository.ProductRepository;
+import com.hugorithm.hopfencraft.repository.RoleRepository;
+import com.hugorithm.hopfencraft.repository.UserRepository;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@TestConfiguration
+public class TestConfig {
+
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final ProductRepository productRepository;
+
+    public TestConfig(
+            RoleRepository roleRepository,
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            ProductRepository productRepository
+    ) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.productRepository = productRepository;
+    }
+
+    @Bean
+    @Primary
+    public DataInitialization testDataInitialization() {
+        return new DataInitialization(
+                roleRepository,
+                userRepository,
+                passwordEncoder,
+                productRepository
+        );
+    }
+}
