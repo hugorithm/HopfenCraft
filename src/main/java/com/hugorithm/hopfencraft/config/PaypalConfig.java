41dfb402a756cb5c76dd677b4b5127ba59e3e592
hugorithm <hugofsilv@gmail.com>
2023-09-12 15:09:13 +0100

    add PaypalConfig

---
+++ src/main/java/com/hugorithm/hopfencraft/config/PaypalConfig.java
@@ -0,0 +1,25 @@
+package com.hugorithm.hopfencraft.config;
+
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PaypalConfig {
+
+    @Value("${paypal.client.id}")
+    private String clientId;
+
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
+
+    @Value("${paypal.mode}")
+    private String mode;
+
+    @Bean
+    public APIContext apiContext() throws PayPalRESTException {
+        return new APIContext(clientId, clientSecret, mode);
+    }
+}
