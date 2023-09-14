ab439969b513d75d9952124c5352e3d1673be10d
hugorithm <hugofsilv@gmail.com>
2023-09-14 23:30:19 +0100

    add PaypalAccessTokenException

---
+++ src/main/java/com/hugorithm/hopfencraft/exception/paypal/PaypalAccessTokenException.java
@@ -0,0 +1,7 @@
+package com.hugorithm.hopfencraft.exception.paypal;
+
+public class PaypalAccessTokenException extends RuntimeException{
+    public PaypalAccessTokenException(String message) {
+        super(message);
+    }
+}
