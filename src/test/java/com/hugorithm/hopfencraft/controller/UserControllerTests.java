7c6c5def6aaa733935828591544f192d25608dca
hugorithm <hugofsilv@gmail.com>
2023-09-15 00:51:05 +0100

    add important TODO for testing

--- src/test/java/com/hugorithm/hopfencraft/controller/UserControllerTests.java
+++ src/test/java/com/hugorithm/hopfencraft/controller/UserControllerTests.java
@@ -42,7 +42,7 @@ public class UserControllerTests {
         // Create a mock Jwt for testing
         mockJwt = Mockito.mock(Jwt.class);
     }
-
+    //TODO: Properly mock this, Instantiate UserService and mock functions accordingly
     @Test
     public void SendPasswordResetRequest_ValidInput_ReturnsOk() throws Exception {
         // Mock the behavior of your userService to return a ResponseEntity
--- src/test/java/com/hugorithm/hopfencraft/service/ProductServiceTests.java
+++ src/test/java/com/hugorithm/hopfencraft/service/ProductServiceTests.java
@@ -25,7 +25,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class ProductServiceTests {
-
     @InjectMocks
     private ProductService productService;
 
