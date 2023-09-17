ad4679852de8dc8b56e59ba2f70923fc259fbcc9
hugorithm <hugofsilv@gmail.com>
2023-09-17 22:44:38 +0100

    add findAllByUser

--- src/main/java/com/hugorithm/hopfencraft/repository/CartItemRepository.java
+++ src/main/java/com/hugorithm/hopfencraft/repository/CartItemRepository.java
@@ -1,9 +1,13 @@
 package com.hugorithm.hopfencraft.repository;
 
+import com.hugorithm.hopfencraft.model.ApplicationUser;
 import com.hugorithm.hopfencraft.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findAllByUser(ApplicationUser user);
 }
