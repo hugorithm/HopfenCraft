ead2b65f908d0cec628e9c51b9df07ee3ab5a0dc
hugorithm <hugofsilv@gmail.com>
2023-09-17 17:36:53 +0100

    add Currency enum

---
+++ src/main/java/com/hugorithm/hopfencraft/enums/Currency.java
@@ -0,0 +1,29 @@
+package com.hugorithm.hopfencraft.enums;
+
+public enum Currency {
+    AUD,
+    BRL,
+    CAD,
+    CNY,
+    CZK,
+    DKK,
+    EUR,
+    HKD,
+    HUF,
+    ILS,
+    JPY,
+    MYR,
+    MXN,
+    TWD,
+    NZD,
+    NOK,
+    PHP,
+    PLN,
+    GBP,
+    SGD,
+    SEK,
+    CHF,
+    THB,
+    USD
+
+}
