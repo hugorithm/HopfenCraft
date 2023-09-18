a659637b1e8867142b5d0f2f8113dd29b12b33e1
hugorithm <hugofsilv@gmail.com>
2023-09-18 17:39:58 +0100

    add Java time module to Jackson Object Mapper

--- src/test/java/com/hugorithm/hopfencraft/utils/JsonToStringConverter.java
+++ src/test/java/com/hugorithm/hopfencraft/utils/JsonToStringConverter.java
@@ -2,11 +2,13 @@ package com.hugorithm.hopfencraft.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JsonToStringConverter {
     // Utility method to convert objects to JSON
     public static String asJsonString(Object obj) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         return objectMapper.writeValueAsString(obj);
     }
 }
