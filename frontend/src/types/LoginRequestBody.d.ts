5ff6d4404d2f660ce87051b850c415702f84db9f
hugorithm <hugofsilv@gmail.com>
2023-09-25 21:22:10 +0100

    add Login types

---
+++ frontend/src/types/LoginRequestBody.d.ts
@@ -0,0 +1,4 @@
+export interface LoginRequestBody {
+    username:    string;
+    password:   string;
+}
---
+++ frontend/src/types/LoginResponse.d.ts
@@ -0,0 +1,6 @@
+export interface LoginResponse {
+    username: string;
+    email:    string;
+    jwt:      string;
+
+}
