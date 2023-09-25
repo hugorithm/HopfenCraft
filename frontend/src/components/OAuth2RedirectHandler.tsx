160914adadef79f01345f7ef8efd720c41cea937
hugorithm <hugofsilv@gmail.com>
2023-09-25 21:22:39 +0100

    add OAuth2 redirect handler

---
+++ frontend/src/components/OAuth2RedirectHandler.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { ACCESS_TOKEN } from '../config/constants';
+import { Navigate, useLocation  } from 'react-router-dom';
+
+
+class OAuth2RedirectHandler extends Component{
+  location = useLocation();
+  
+  getUrlParameter(name: string) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+
+    const results = regex.exec(location.search);
+
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  }
+
+  render() {
+    const token = this.getUrlParameter('token');
+    const error = this.getUrlParameter('error');
+
+    if (token) {
+      localStorage.setItem(ACCESS_TOKEN, token);
+
+      return (
+        <Navigate to="/profile" />
+      );
+    } else {
+      return (
+        <Navigate to="/login" />
+      );
+    }
+  }
+}
+
+export default OAuth2RedirectHandler;
