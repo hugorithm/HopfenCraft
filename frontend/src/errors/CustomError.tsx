41c8a02e1958a525dd6a19fe160a87e6a5745f29
hugorithm <hugofsilv@gmail.com>
2023-09-26 01:02:54 +0100

    update custom error

--- frontend/src/errors/CustomError.tsx
+++ frontend/src/errors/CustomError.tsx
@@ -1,34 +1,52 @@
 import React from 'react';
-import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { CssBaseline } from '@mui/material';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 const CustomError = () => {
   return (
     <>
-    <CssBaseline />
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      height="100vh"
-    >
-      <Typography variant="h4" color="error" gutterBottom>
-        Oops! Something went wrong.
-      </Typography>
-      <Typography variant="body1" paragraph>
-        We apologize, but an error occurred while loading the page.
-      </Typography>
-      <Typography variant="body1" paragraph>
-        Please try again later or contact support if the issue persists.
-      </Typography>
-      <Button variant="contained" color="primary" onClick={() => window.location.reload()}>
-        Refresh Page
-      </Button>
-    </Box>
-    
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box
+          sx={{
+            mt: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <ErrorOutlineIcon
+            color="error"
+            sx={{ fontSize: 80, marginBottom: '16px' }}
+          />
+          <Typography variant="h4" color="error" gutterBottom>
+            Oops! Something went wrong.
+          </Typography>
+          <Typography variant="body1" paragraph>
+            We apologize, but an error occurred while loading the page.
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Please try again later or contact support if the issue persists.
+          </Typography>
+          <img
+            src="/src/assets/errors/oops.gif"
+            alt="404 Not Found"
+            height="300"
+            style={{ marginBottom: '16px' }}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => window.location.reload()}
+          >
+            Refresh Page
+          </Button>
+        </Box>
+      </Container>
     </>
   );
 }
