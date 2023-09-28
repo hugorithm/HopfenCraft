927c9c16f013d8e036851b0efbb81974479f5b36
hugorithm <hugofsilv@gmail.com>
2023-09-28 21:51:44 +0100

    add total field to CartItemDTO

--- backend/src/main/java/com/hugorithm/hopfencraft/dto/cart/CartItemDTO.java
+++ backend/src/main/java/com/hugorithm/hopfencraft/dto/cart/CartItemDTO.java
@@ -1,10 +1,12 @@
 package com.hugorithm.hopfencraft.dto.cart;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.hugorithm.hopfencraft.dto.product.ProductDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
@@ -14,5 +16,7 @@ public class CartItemDTO {
     private Long cartItemId;
     private ProductDTO product;
     private int quantity;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private BigDecimal total;
     private LocalDateTime addedDateTime;
 }
