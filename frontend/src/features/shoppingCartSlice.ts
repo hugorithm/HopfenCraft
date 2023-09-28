e3043890dc678fd5d344f23a17fb9c05c72de4a2
hugorithm <hugofsilv@gmail.com>
2023-09-28 16:27:33 +0100

    add shoppingCartSlice

---
+++ frontend/src/features/shoppingCartSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { CartItem, ShoppingCartResponse } from '../types/ShoppingCartResponse';
+
+export interface ShoppingCartState {
+  cartItems:  CartItem[];
+}
+
+const initialState: ShoppingCartState = {
+    cartItems: []
+}
+
+export const shoppingCartSlice = createSlice({
+  name: "shoppingCart",
+  initialState,
+  reducers: {
+    setCartItems: (state, action: PayloadAction<ShoppingCartResponse>) => {
+      state.cartItems = action.payload.cartItems;
+    },
+   
+  },
+});
+
+type ShoppingCartSelector = (state: { shoppingCart: ShoppingCartState }) => ShoppingCartState;
+export const selectAuth: ShoppingCartSelector = (state) => state.shoppingCart;
+export const { setCartItems } = shoppingCartSlice.actions;
+export default shoppingCartSlice.reducer;
