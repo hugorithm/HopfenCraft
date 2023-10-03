d8b3290c36a3c4f273ca4ddc5290508d16c5e656
hugorithm <hugofsilv@gmail.com>
2023-10-03 14:18:56 +0100

    update store

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,25 +1,36 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { authApi } from './api/auth/authApi';
 import { setupListeners } from '@reduxjs/toolkit/query/react'
-import authReducer  from '../features/authSlice';
-import productsReducer from '../features/productsSlice'; 
+import authReducer from '../features/authSlice';
+import productsReducer from '../features/productsSlice';
 import shoppingCartReducer from '../features/shoppingCartSlice';
+import orderReducer from '../features/orderSlice';
 import { shoppingCartApi } from "./api/shoppingCartApi";
 import { productApi } from "./api/productApi";
 import { rtkQueryErrorLogger } from "./middleware/ErrorHandler";
+import { orderApi } from "./api/orderApi";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   [authApi.reducerPath]: authApi.reducer,
   [shoppingCartApi.reducerPath]: shoppingCartApi.reducer,
-  [productApi.reducerPath] : productApi.reducer,
+  [productApi.reducerPath]: productApi.reducer,
+  [orderApi.reducerPath]: orderApi.reducer,
   shoppingCart: shoppingCartReducer,
-  products: productsReducer, 
+  order: orderReducer,
+  products: productsReducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware, shoppingCartApi.middleware, productApi.middleware, rtkQueryErrorLogger),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(
+      authApi.middleware,
+      shoppingCartApi.middleware,
+      productApi.middleware,
+      orderApi.middleware,
+      rtkQueryErrorLogger
+    ),
 });
 
 
