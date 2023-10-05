7f0e9b9c9cfc908da3c57a21d67f02fc05c164d3
hugorithm <hugofsilv@gmail.com>
2023-10-06 00:38:24 +0100

    add some utils

---
+++ frontend/src/utils/dateFormatter.ts
@@ -0,0 +1,12 @@
+const dateFormater = (dateString: string): string => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
+}
+
+export default dateFormater;
---
+++ frontend/src/utils/priceFormatter.ts
@@ -0,0 +1,5 @@
+const formatPrice = (num: number): string => {
+    return `${num.toFixed(2)}`;
+  }
+
+export default formatPrice;
