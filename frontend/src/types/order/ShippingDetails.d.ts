25b516aa6e52f53d32fc0bea6f2a890c744e514c
hugorithm <hugofsilv@gmail.com>
2023-10-05 20:44:09 +0100

    add ShippingDetails type

---
+++ frontend/src/types/order/ShippingDetails.d.ts
@@ -0,0 +1,14 @@
+export interface ShippingDetails {
+    shippingName:       string;
+    shippingAddress:    string;
+    shippingCity:       string;
+    shippingState:      string;
+    shippingPostalCode: string;
+    shippingCountry:    string;
+    billingName:        string;
+    billingAddress:     string;
+    billingCity:        string;
+    billingState:       string;
+    billingPostalCode:  string;
+    billingCountry:     string;
+}
