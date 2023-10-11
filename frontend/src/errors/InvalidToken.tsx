5d7bdaaa3a6db3e48fa50029d9347f3a00bc5bbd
hugorithm <hugofsilv@gmail.com>
2023-10-12 00:18:04 +0100

    add InvalidToken Component

---
+++ frontend/src/errors/InvalidToken.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { CssBaseline } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+
+const InvalidToken = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box
+          sx={{
+            mt: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <ErrorOutlineIcon
+            color="error"
+            sx={{ fontSize: 80, marginBottom: '16px' }}
+          />
+          <Typography variant="h4" color="error" gutterBottom>
+           Invalid Token!
+          </Typography>
+          <Typography variant="body1" paragraph>
+            The token you provided is invalid.
+          </Typography>
+          <Button
+              variant="contained"
+              sx={{ mt: 2 }}
+              component={RouterLink}
+              to="/home"
+            >
+              Go home
+            </Button>
+        </Box>
+      </Container>
+    </>
+  );
+}
+
+export default InvalidToken;
