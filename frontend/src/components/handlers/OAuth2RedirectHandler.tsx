26c4cc33275fcc166b6bb2c1cd4194cad0eb47b6
hugorithm <hugofsilv@gmail.com>
2023-10-12 00:17:42 +0100

    move handler components into handlers folder

---
+++ frontend/src/components/handlers/OAuth2RedirectHandler.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useAppDispatch } from '../../app/hooks';
+import { setUser } from '../../features/authSlice';
+import { useGetOAuth2UserQuery } from '../../app/api/auth/authApi';
+import { useThemeContext } from '../../theme/ThemeContextProvider';
+import { toast } from 'react-toastify';
+
+const OAuth2RedirectHandler = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const token = new URLSearchParams(location.search).get("token") ?? "";
+  const error = new URLSearchParams(location.search).get("error");
+  const { data, isSuccess, isError, error: oAuth2Error } = useGetOAuth2UserQuery(token);
+  const { mode }: any = useThemeContext();
+
+  useEffect(() => {
+    if (token) {
+      if (data && isSuccess) {
+        dispatch(setUser({
+          username: data.name, email: data.email, jwt: token, roles: []
+        }));
+        toast.success('Login Successful!', {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          pauseOnFocusLoss: false,
+          progress: undefined,
+          theme: mode,
+        });
+        navigate("/profile");
+      }
+    } else {
+      navigate("/login");
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (error) {
+      console.error(error);
+      navigate("/login");
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (isError) {
+      console.error(oAuth2Error);
+      navigate("/login");
+    }
+  }, [isError]);
+
+  return null;
+}
+
+export default OAuth2RedirectHandler;
---
+++ frontend/src/components/handlers/PasswordResetHandler.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useValidateTokenQuery } from '../../app/api/passwordResetApi';
+
+const PasswordResetHandler = () => {
+  const location = useLocation();
+  const token = new URLSearchParams(location.search).get("token") ?? "";
+  const navigate = useNavigate();
+  const encodedToken = encodeURIComponent(token).replace(".", "%2E");
+  localStorage.setItem("resetToken", encodedToken);
+  const {isSuccess, isError, error} = useValidateTokenQuery(encodedToken);
+
+  useEffect(() => {
+    if (isError) {
+      console.error(error);
+      navigate("/invalid-token");
+    }
+  }, [isError]);
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate("/user/reset-password-form");
+    }
+  }, [isSuccess]);
+
+  return null;
+}
+
+export default PasswordResetHandler;
