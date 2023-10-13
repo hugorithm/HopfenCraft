71050f1b46fe6392c128decfdad028f735edc7f5
hugorithm <hugofsilv@gmail.com>
2023-10-14 00:51:37 +0100

    add method for building paypal payment success message

--- backend/src/main/java/com/hugorithm/hopfencraft/service/EmailService.java
+++ backend/src/main/java/com/hugorithm/hopfencraft/service/EmailService.java
@@ -46,7 +46,132 @@ public class EmailService {
         }
     }
 
-    public String buildWelcomeEmail(String username) {
+    private String buildPaypalPaymentSuccessEmail(ApplicationUser user, Order order) {
+        StringBuilder orderItemsDetails = new StringBuilder();
+
+        for (OrderItem orderItem : order.getOrderItems()) {
+            Product product = orderItem.getProduct();
+
+            orderItemsDetails.append("<strong>Brand:</strong> ")
+                    .append(product.getBrand())
+                    .append("<br>\n")
+                    .append("<strong>Product:</strong> ")
+                    .append(product.getName())
+                    .append("<br>\n")
+                    .append(orderItem.getQuantity())
+                    .append(" x €")
+                    .append(product.getPrice())
+                    .append(" = €")
+                    .append(orderItem.getTotal())
+                    .append("<br><br>\n\n");
+        }
+        return "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "    <title>Payment Success at HopfenCraft</title>\n" +
+                "    <style>\n" +
+                "        /* Reset email styles */\n" +
+                "        body {\n" +
+                "            font-family: Arial, sans-serif;\n" +
+                "            background-color: #f7f7f7;\n" +
+                "            margin: 0;\n" +
+                "            padding: 0;\n" +
+                "        }\n" +
+                "\n" +
+                "        /* Container */\n" +
+                "        .container {\n" +
+                "            max-width: 600px;\n" +
+                "            margin: 0 auto;\n" +
+                "            padding: 20px;\n" +
+                "            background-color: #ffffff;\n" +
+                "            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);\n" +
+                "        }\n" +
+                "\n" +
+                "        /* Header */\n" +
+                "        .header {\n" +
+                "            text-align: center;\n" +
+                "            padding: 20px 0;\n" +
+                "        }\n" +
+                "\n" +
+                "        .header h1 {\n" +
+                "            color: #333333;\n" +
+                "            font-size: 24px;\n" +
+                "        }\n" +
+                "\n" +
+                "        /* Content */\n" +
+                "        .content {\n" +
+                "            display: flex;\n" +
+                "            flex-wrap: wrap;\n" +
+                "            justify-content: center;\n" +
+                "            background-color: #ffffff;\n" +
+                "            padding: 30px;\n" +
+                "            border-radius: 4px;\n" +
+                "            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);\n" +
+                "        }\n" +
+                "\n" +
+                "        .message {\n" +
+                "            width: 100%;\n" +
+                "            font-size: 16px;\n" +
+                "            color: #333333;\n" +
+                "            margin-bottom: 20px;\n" +
+                "        }\n" +
+                "\n" +
+                "        .product-details {\n" +
+                "            width: 100%;\n" +
+                "            font-size: 16px;\n" +
+                "            color: #333333;\n" +
+                "            margin-bottom: 20px;\n" +
+                "        }\n" +
+                "\n" +
+                "        /* Creative Image */\n" +
+                "        .creative-image {\n" +
+                "            text-align: center;\n" +
+                "        }\n" +
+                "\n" +
+                "        .creative-image img {\n" +
+                "            max-width: 100%;\n" +
+                "            height: auto;\n" +
+                "        }\n" +
+                "\n" +
+                "        /* Footer */\n" +
+                "        .footer {\n" +
+                "            text-align: center;\n" +
+                "            padding-top: 30px;\n" +
+                "            color: #888888;\n" +
+                "            font-size: 12px;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    <div class=\"container\">\n" +
+                "        <div class=\"header\">\n" +
+                "            <h1>Payment Success at HopfenCraft</h1>\n" +
+                "        </div>\n" +
+                "        <div class=\"content\">\n" +
+                "            <p class=\"message\">Hi <strong>" + user.getUsername() + "</strong>,</p>\n" +
+                "            <p class=\"message\">Your payment was successful for the following product:</p>\n" +
+                "            <div class=\"product-details\">\n" +
+                orderItemsDetails +
+                "            </div>\n" +
+                "            <p class=\"message\"><strong>Total:</strong> €" + order.getTotal() + "</p>" +
+                "            <p class=\"message\">Thank you for your purchase. Enjoy your beers! Cheers! 🍻</p>\n" +
+                "            <div class=\"creative-image\">\n" +
+                "                <!--img src=\"file.png\" alt=\"HopfenCraft Illustration\"-->\n" +
+                "            </div>\n" +
+                "        </div>\n" +
+                "        <div class=\"footer\">\n" +
+                "            <p>If you have any questions or need further assistance, feel free to <a href=\"mailto:support@hopfencraft.com\">contact us</a>.</p>\n" +
+                "            <p>&copy; " + LocalDateTime.now().getYear() + " HopfenCraft</p>\n" +
+                "        </div>\n" +
+                "    </div>\n" +
+                "</body>\n" +
+                "</html>";
+    }
+
+
+    private String buildWelcomeEmail(ApplicationUser user) {
         return "<!DOCTYPE html>\n" +
                 "<html lang=\"en\">\n" +
                 "<head>\n" +
