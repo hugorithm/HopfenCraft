f7c8e7dbb35a36e2ef58a1630af86771830a6b18
hugorithm <hugofsilv@gmail.com>
2023-10-16 22:00:03 +0100

    add updateCartItemQuantity method to ShoppingCartController

--- backend/src/main/java/com/hugorithm/hopfencraft/controller/ShoppingCartController.java
+++ backend/src/main/java/com/hugorithm/hopfencraft/controller/ShoppingCartController.java
@@ -1,5 +1,6 @@
 package com.hugorithm.hopfencraft.controller;
 
+import com.hugorithm.hopfencraft.dto.cart.CartItemQuantityUpdateDTO;
 import com.hugorithm.hopfencraft.dto.cart.CartRegistrationDTO;
 import com.hugorithm.hopfencraft.dto.cart.CartResponseDTO;
 import com.hugorithm.hopfencraft.service.ShoppingCartService;
@@ -11,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("/cart")
@@ -32,10 +32,15 @@ public class ShoppingCartController {
         return shoppingCartService.addToCart(jwt, body);
     }
 
+    @PutMapping("/update")
+    @RolesAllowed("USER")
+    public ResponseEntity<CartResponseDTO> updateCartItemQuantity(@AuthenticationPrincipal Jwt jwt, @Valid @RequestBody CartItemQuantityUpdateDTO body) {
+        return shoppingCartService.updateCartItemQuantity(jwt, body);
+    }
+
     @DeleteMapping("/remove/{cartItemId}")
     @RolesAllowed("USER")
     public ResponseEntity<CartResponseDTO> removeCartItem(@AuthenticationPrincipal Jwt jwt, @PathVariable @Positive Long cartItemId) {
         return shoppingCartService.removeCartItem(jwt, cartItemId);
     }
-
 }
---
+++ backend/src/main/java/com/hugorithm/hopfencraft/dto/cart/CartItemQuantityUpdateDTO.java
@@ -0,0 +1,2 @@
+package com.hugorithm.hopfencraft.dto.cart;public class CartItemQuantityUpdateDTO {
+}
