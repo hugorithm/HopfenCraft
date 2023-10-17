1cb47ac877d0adf4bb5298c9c7ffff11f6b092f4
hugorithm <hugofsilv@gmail.com>
2023-10-17 21:15:20 +0100

    add positive validation for quantity and id

--- backend/src/main/java/com/hugorithm/hopfencraft/dto/cart/CartItemQuantityUpdateDTO.java
+++ backend/src/main/java/com/hugorithm/hopfencraft/dto/cart/CartItemQuantityUpdateDTO.java
@@ -1,5 +1,6 @@
 package com.hugorithm.hopfencraft.dto.cart;
 
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class CartItemQuantityUpdateDTO {
+    @Positive
     private Long cartItemId;
+    @Positive
     private int quantity;
 }
