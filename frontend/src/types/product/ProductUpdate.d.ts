2c9756288004fee5ebcd93fbf09e8f68916b9670
hugorithm <hugofsilv@gmail.com>
2023-10-18 21:40:08 +0100

    add product update request type

---
+++ frontend/src/types/product/ProductUpdate.d.ts
@@ -0,0 +1,8 @@
+export interface ProductUpdate {
+    productId:    number;
+    brand:       string;
+    name:        string;
+    description: string;
+    quantity:    string;
+    price:       string;
+}
