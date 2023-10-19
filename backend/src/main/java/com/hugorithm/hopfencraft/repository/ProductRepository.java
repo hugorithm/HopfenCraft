6605a8938dc42635888579728119ce94333e6c4c
hugorithm <hugofsilv@gmail.com>
2023-10-19 20:49:08 +0100

    add sql query to fetch product based on search keyword

--- backend/src/main/java/com/hugorithm/hopfencraft/repository/ProductRepository.java
+++ backend/src/main/java/com/hugorithm/hopfencraft/repository/ProductRepository.java
@@ -1,13 +1,20 @@
 package com.hugorithm.hopfencraft.repository;
 
 import com.hugorithm.hopfencraft.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findProductByName(String name);
     Optional<Product> findProductByBrandName(String brandName);
+    @Query("select p from Product p where LOWER(p.brand) like %:keyword% or LOWER(p.name) like %:keyword%")
+    Page<Product> findByProductNameOrDescription(@Param("keyword") String keyword, Pageable pageable);
 }
