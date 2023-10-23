6f204ee817190562ccccaf798e62761c97520133
hugorithm <hugofsilv@gmail.com>
2023-10-23 20:24:48 +0100

    Add sku to Product type

--- frontend/src/types/product/ProductData.d.ts
+++ frontend/src/types/product/ProductData.d.ts
@@ -18,6 +18,7 @@ export interface Product {
     name:             string;
     description:      string;
     quantity:         number;
+    sku:              string;
     price:            string;
     currency:         Currency;
     registerDateTime: string;
