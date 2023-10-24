934dafa87a8fd41841daf607f351a50238b0d724
hugorithm <hugofsilv@gmail.com>
2023-10-25 00:03:08 +0100

    refactor: remove unused imports

--- frontend/src/errors/CustomError.tsx
+++ frontend/src/errors/CustomError.tsx
@@ -3,7 +3,6 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import { CssBaseline } from '@mui/material';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 const CustomError = () => {
