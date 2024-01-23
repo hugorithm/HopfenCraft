c3e1e35008841b905c73db0549fe5c89c301f045
hugorithm <hugofsilv@gmail.com>
2024-01-23 18:14:40 +0000

    Add host and port config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    strictPort: true,
+    port: 3000,
     watch: {
       usePolling: true,
     }
